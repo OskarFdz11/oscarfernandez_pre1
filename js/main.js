57d551df5a5403f9c2ab7bdb7203808732308c4e
oscarfdz <oskar.fdz111@gmail.com>
2024-07-17 15:48:19 -0600

    cambios finales

--- js/main.js
+++ js/main.js
@@ -8,13 +8,13 @@ function test() {
 
     const preguntas = [
 
-        "1- Sentirse nervioso/a, intranquilo/a o con los nervios de punta",
-        "2- No poder dejar de preocuparse o no poder controlar la preocupación",
-        "3- Preocuparse demasiado por diferentes cosas",
+        "1- Sentirse nervioso/a, intranquilo/a o con los nervios de punta. 0 - nunca, 1 - varios días, 2 - más de la mitad de los días, 3 - casi todos los días",
+        "2- No poder dejar de preocuparse o no poder controlar la preocupación. 0 - nunca, 1 - varios días, 2 - más de la mitad de los días, 3 - casi todos los días",
+        "3- Preocuparse demasiado por diferentes cosas. 0 - nunca, 1 - varios días, 2 - más de la mitad de los días, 3 - casi todos los días",
         "4- Dificultad para relajarse",
-        "5- Estar inquieto/a que es difícil permanecer sentado/a tranquilamente",
-        "6- Molestarse o ponerse irritable fácilmente",
-        "7- Sentir miedo como si algo terrible pudiera pasar"
+        "5- Estar inquieto/a que es difícil permanecer sentado/a tranquilamente. 0 - nunca, 1 - varios días, 2 - más de la mitad de los días, 3 - casi todos los días",
+        "6- Molestarse o ponerse irritable fácilmente. 0 - nunca, 1 - varios días, 2 - más de la mitad de los días, 3 - casi todos los días",
+        "7- Sentir miedo como si algo terrible pudiera pasar. 0 - nunca, 1 - varios días, 2 - más de la mitad de los días, 3 - casi todos los días"
 
     ];
 
@@ -23,6 +23,7 @@ function test() {
 
     // se inicia un bucle for para iterar cada una de las preguntas del array
     for (let i = 0; i < preguntas.length; i++) {
+
         let valor; //creamos la variable que contendrá cada una de las respuestas que el usario ingresa
         while (true) { 
             valor = prompt(preguntas[i]);
@@ -69,7 +70,7 @@ function test() {
 //seleccionamos el botón que se encuentra en el index para que al hacer click inicie con el test
 const startButton = document.querySelector(".start");
 
-startButton.addEventListener("click", test);
+startButton.addEventListener("click", test); 
 
 
 
